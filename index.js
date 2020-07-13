5974ec8500e9e02b910bb11799d965bdc006ded7
Augusto Peres <augustoapg@gmail.com>
2020-07-13 18:31:33 -0400

    created Transaction object with backend validation

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const server = express();
+const Transaction = require('./models/transaction');
+
+server.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
 server.get('/', (req, res) => {
     res.send('Backend root');
 });
 
 server.post('/newTransaction', (req, res) => {
-    console.log(req);
-    res.json({message: 'New Transaction Saved!'});
+    const {type, who, category, title, date, value, notes} = req.body;
+
+    console.log(type)
+
+    try {
+        const newTransaction = new Transaction(type, who, category, title, date, value, notes);
+        console.log('Object created');
+        res.send('Success!');
+    } catch (e) {
+        console.log(e)
+        res.send(e);
+    }
+    console.log(req.body);
+    
 });
 
 server.listen(4242, () => {
---
+++ models/transaction.js
@@ -0,0 +1,103 @@
+/**
+ * 
+ */
+
+class Transaction {
+    constructor(type, who, category, title, date, value, notes) {
+        this._id = this.getNewId();
+        this._type = type;
+        this._who = who;
+        this._category = category;
+        this._title = title;
+        this._date = date;
+        this._value = value;
+        this._notes = notes;
+    }
+
+    getNewId() {
+        return 1;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    set type(value) {
+        if (value) {
+            this._type = value;
+        } else {
+            throw new Error('Type cannot be empty');
+        }
+    }
+
+    get who() {
+        return this._who;
+    }
+
+    set who(value) {
+        if (value) {
+            this._who = value;
+        } else {
+            throw new Error('Who cannot be empty');
+        }
+    }
+
+    get category() {
+        return this._category;
+    }
+
+    set category(value) {
+        if (value) {
+            this._category = value;
+        } else {
+            throw new Error('category cannot be empty');
+        }
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(value) {
+        if (value) {
+            this._title = value;
+        } else {
+            throw new Error('title cannot be empty');
+        }
+    }
+
+    get date() {
+        return this._date;
+    }
+
+    set date(value) {
+        if (value) {
+            this._date = value;
+        } else {
+            throw new Error('date cannot be empty');
+        }
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(value) {
+        if (value) {
+            this._value = value;
+        } else {
+            throw new Error('value cannot be empty');
+        }
+    }
+
+    get notes() {
+        return this._notes;
+    }
+
+    set notes(value) {
+        this._notes = value;
+    }
+}
+
+module.exports = Transaction;
+
